b4f3655824274cb4fa418a536f967bb9e18fbe78
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-04-20 00:42:16 -0700

    Commited-API-cryptos-called

--- CryptoTrackerApp/src/components/CryptoApi.tsx
+++ CryptoTrackerApp/src/components/CryptoApi.tsx
@@ -1,23 +1,43 @@
 import { useState, useEffect } from 'react'
+import '../css/cryptoApi.css'
+
 // API Key
 
-const cryptoApiKey = 'CG-cau3wxcR1wSjs9iw2Uv6V8Up'
+const cryptoApiKey = 'CG-aUMCTa9KS1trBrKPu1iip2q8'
 
 export function CryptoApi() {
   const [crytoMappedApi, setCrytoMappedApi] = useState<CryptoMappedImagesType>()
 
-  //   type
+  //   type validation for tsx
 
   type CryptoMappedImagesType = [
     {
-      content: string
       id: string
-      market_cap: number
-      market_cap_change_24h: number
+      symbol: string
       name: string
-      top_3_coins: string[]
-      updated_at: string
-      volume_24th: string
+      image: undefined
+      current_price: number
+      market_cap: number
+      market_cap_rank: number
+      fully_diluted_valuation: number
+      total_volume: number
+      high_24h: number
+      low_24h: number
+      price_change_24h: number
+      price_change_percentage_24h: number
+      market_cap_change_24h: number
+      market_cap_change_percentage_24h: number
+      circulating_supply: number
+      total_supply: number
+      max_supply: number
+      ath: number
+      ath_change_percentage: number
+      ath_date: string
+      atl: number
+      atl_change_percentage: number
+      atl_date: string
+      roi: {} | null
+      last_updated: string
     },
   ]
 
@@ -25,7 +45,7 @@ export function CryptoApi() {
     async function cryptoCatagoriesImgFetch() {
       try {
         const response = await fetch(
-          `https://api.coingecko.com/api/v3/coins/categories?order=market_cap_desc `,
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd',
         )
 
         const data = await response.json()
@@ -42,13 +62,25 @@ export function CryptoApi() {
 
   return (
     <>
-      {/* <div className="hidden">
-        {crytoMappedApi?.map((crytos, index) => (
-          <img key={index} src={crytos.top_3_coins[0]} alt="" />
+      <div className="flex flex-col items-end">
+        {crytoMappedApi?.map((cryptos, index) => (
+          <>
+            <div className="flex items-center">
+              <h3 className="text-2xl">{`${cryptos.market_cap_rank})`}</h3>
+              <img
+                src={cryptos.image}
+                className="crytoImageHeight object-contain capitalize p-7"
+                alt={`${cryptos.symbol}.`}
+                title={cryptos.id}
+              />
+            </div>
+            <div className="block mx-auto">
+              <p className="text-center">{cryptos.id}</p>
+            </div>
+            <hr />
+          </>
         ))}
-      </div> */}
-
-      <h1 className="text-blue-500">dwjsk</h1>
+      </div>
     </>
   )
 }
